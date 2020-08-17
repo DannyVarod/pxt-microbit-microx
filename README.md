2c0363774fb167189cd20e4396f91e78a2f7bd6e
DannyVarod <dvarod@digibank.co.il>
2020-08-18 00:24:00 +0300

    add support for rgb leds

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 An experiment extension for MakeCode and micro:bit supporting various hardware extenions.
 I will copy the working blocks into a different stable repository (name TBD).
+
+Currently includes support for (in development):
+
+* Yahboom remote / game handle
+* Kittenbot Powerblock / Armorbit
+* Kittenbot Robotbit
--- main.ts
+++ main.ts
@@ -8,7 +8,13 @@ namespace microX {
 
     const PWM_PCA9685_ADDRESS = 0x40
 
-    let wasInitializedPhaseWidthModulationDriver = false
+    let initializedPhaseWidthModulationDriver = false
+    let initializedPowerbrickRgb = false
+    let initializedRobotbit = false
+    let initializedYBRemote = false
+
+    let powerblockPixels: RgbMatrix = null
+    let robotbitPixels: RgbMatrix = null
 
     /**
      * Servo number (1 to 8)
@@ -58,9 +64,31 @@ namespace microX {
     }
 
     /**
+     * PowerbrickDigitalPorts
+     */
+    const PowerbrickDigitalPorts = [
+        [DigitalPin.P8, DigitalPin.P0],
+        [DigitalPin.P12, DigitalPin.P1],
+        [DigitalPin.P13, DigitalPin.P2],
+        [DigitalPin.P15, DigitalPin.P14],
+        [DigitalPin.P6, DigitalPin.P3],
+        [DigitalPin.P7, DigitalPin.P4],
+        [DigitalPin.P9, DigitalPin.P10]
+    ]
+
+    function inRange(x: number, minValue: number, maxValue: number): number {
+        if (x < minValue)
+            x = minValue
+        else if (x > maxValue)
+            x = maxValue
+        return x
+    }
+
+    /**
      * Get X-axis value of YB-EMH02 ver 1.2 joystick (port2) between -1 (left) and 1 (right)
      */
     //% block="joystickX"
+    //% group="Yahboom remote"
     export function joystickX(): number {
         
         // Normalize from [0,1023] to [-512,511]
@@ -78,6 +106,7 @@ namespace microX {
      * Get Y-axis value of YB-EMH02 ver 1.2 joystick (port1) between -1 (bottom) and 1 (top)
      */
     //% block="joystickY"
+    //% group="Yahboom remote"
     export function joystickY() {
         
         // Normalize from [0,1023] to [-512,511]
@@ -91,13 +120,50 @@ namespace microX {
         return readValue / 512.0
     }
 
-    //% block="initialize YB remote buttons to pullup"
-    export function initializeYBRemoteButtons(): void {
+    //% block="initialize for Yahboom remote"
+    //% group="Yahboom remote"
+    export function initializeYBRemote(): void {
+        if (initializedYBRemote)
+            return
         pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
         pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+        initializePhaseWidthModulationDriver()
+        initializedYBRemote = true
+    }
+
+    //% blockId=intializePowerbrickPixels block="initialize powerbit pixels|%port"
+    //% weight=99
+    //% port.min=1 port.max=7
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% blockId=intializePowerbrickRgb block="Initialize powerbrick pixels"
+    //% group="Powerbrick pixels"
+    export function intializePowerbrickRgb(port: number): boolean {
+        if (initializedPowerbrickRgb)
+            return true
+
+        if (port < 1 || port > 7)
+            return false
+        
+        powerblockPixels = new RgbMatrix(8, 8, PowerbrickDigitalPorts[port - 1][0])
+        
+        initializedPowerbrickRgb = true
+        return true
+    }
+
+    //% block="initialize for Kittenbot Robotbit"
+    //% group="Robotbit"
+    export function intializeRobotbit(): void {
+        if (initializedRobotbit)
+            return
+        
+        initializePhaseWidthModulationDriver()
+        
+        robotbitPixels = new RgbMatrix(1, 4, DigitalPin.P16)
+        
+        initializedRobotbit = true
     }
 
     /**
@@ -106,6 +172,7 @@ namespace microX {
     //% block="onYBRemoteButton %pin |%state"
     //% blockGap=10
     //% name.fieldEditor="gridpicker name.fieldOptions.columns=5
+    //% group="Yahboom remote"
     export function onYBRemoteButton(button: YBRemoteButton, state: ButtonState, body: Action): void {
         
         let pulseValue: PulseValue = PulseValue.Low
@@ -163,19 +230,22 @@ namespace microX {
     }
 
     //% block="initialize PWM driver (for servos and motors)"
-    export function initPhaseWidthModulationDriver(): void {
-        if (wasInitializedPhaseWidthModulationDriver)
+    //% group="PhaseWidthModulation"
+    export function initializePhaseWidthModulationDriver(): void {
+        if (initializedPhaseWidthModulationDriver)
             return
         i2cwrite(PWM_PCA9685_ADDRESS, 0x00, 0x00)
         // Operate at 50Hz
         setFreq(50)
-        wasInitializedPhaseWidthModulationDriver = true
+        initializedPhaseWidthModulationDriver = true
     }
 
     function setPwm(channel: number, low2Bytes: number, high2Bytes: number): void {
         if (channel < 0 || 15 < channel)
             return
-        initPhaseWidthModulationDriver()
+        
+        initializePhaseWidthModulationDriver()
+        
         let buffer = pins.createBuffer(5)
         buffer[0] = (channel << 2) + 6
         buffer[1] = low2Bytes & 0xff
@@ -194,18 +264,15 @@ namespace microX {
     //% weight=85
     //% speed.min=-4096 speed.max=4096
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="PhaseWidthModulation"
     export function MotorRun(motorNum: Motor, speed: number): void {
         if (motorNum < 0 || 3 < motorNum)
             return
-        initPhaseWidthModulationDriver()
+        
+        initializePhaseWidthModulationDriver()
         
         let speed1 = speed
-        if (speed1 > 4096) {
-            speed1 = 4096
-        }
-        else if (speed1 < -4096) {
-            speed1 = -4096
-        }
+        speed1 = inRange(speed1, -4096, 4096)
 
         let speed2 = 0
         if (speed1 < 0) {
@@ -228,14 +295,12 @@ namespace microX {
     //% weight=99
     //% pulseWidth.min=1 pulseWidth.max=19999
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="PhaseWidthModulation"
     export function setServoPulseWidth(servoNum: Servo, pulseWidth: number): void {
-        initPhaseWidthModulationDriver()
+        initializePhaseWidthModulationDriver()
         // 50Hz --> Full cycle is 20mS (20000uS), normalize this from range 0...20000uS to 0...4096
         let value = Math.round(pulseWidth * 4096.0 / 20000.0)
-        if (value < 0)
-            value = 0
-        else if (value > 4096)
-            value = 4096
+        value = inRange(value, 0, 4096)
         // Servos are mapped to channels [8, 9, 10, 11, 12, 13, 14, 15]
         setPwm(servoNum + 7, 0, value)
     }
@@ -249,13 +314,11 @@ namespace microX {
     //% weight=99
     //% speed.min=-1000 speed.max=1000
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="PhaseWidthModulation"
     export function setOrangeGreenGeekservoSpeed(servoNum: Servo, speed: number): void {
         // reverse: 500uS-1500uS, 0: 1500uS, forward: 1500uS-2500uS
         let pulseWidth = 1500 + speed
-        if (pulseWidth > 2500)
-            pulseWidth = 2500
-        else if (pulseWidth < 500)
-            pulseWidth = 500
+        pulseWidth = inRange(pulseWidth, 500, 2500)
         setServoPulseWidth(servoNum, pulseWidth)
     }
 
@@ -268,6 +331,7 @@ namespace microX {
     //% weight=99
     //% degree.min=-45 degree.max=225
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="PhaseWidthModulation"
     export function setGreyGeekservoAngle(servoNum: Servo, degree: number): void {
         // -45deg: 600uS, 225deg: 2400uS (6.6667 uS/deg = 20.0/3.0)
         let degree_norm = (degree + 45.0) % 360
@@ -280,10 +344,7 @@ namespace microX {
                 degree_norm = 0
         }
         let pulseWidth = degree_norm * 20.0 / 3.0 + 600
-        if (pulseWidth > 2400)
-            pulseWidth = 2400
-        else if (pulseWidth < 600)
-            pulseWidth = 600
+        pulseWidth = inRange(pulseWidth, 600, 2400)
         setServoPulseWidth(servoNum, pulseWidth)
     }
 
@@ -296,6 +357,7 @@ namespace microX {
     //% weight=99
     //% degree.min=0 degree.max=360
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="PhaseWidthModulation"
     export function setLargeGreygGeekservoAngle(servoNum: Servo, degree: number): void {
         // TODO: When I have a servo like this, check if this is 0 to 360 or 0 to 350 and if only goes to 350 degress, replace constants
         // 0deg: 500uS, 360deg: 2500uS
@@ -303,11 +365,177 @@ namespace microX {
         if (degree_norm < 0)
             degree_norm += 360
         let pulseWidth = degree_norm * 2000.0 / 360.0 + 500
-        if (pulseWidth > 2500)
-            pulseWidth = 2500
-        else if (pulseWidth < 500)
-            pulseWidth = 500
+        pulseWidth = inRange(pulseWidth, 500, 2500)
         setServoPulseWidth(servoNum, pulseWidth)
+    }
+
+    export class RgbMatrix {
+        rows: number
+        columns: number
+        port: DigitalPin
+        displayBuffer: Buffer
+
+        constructor(rows: number, columns: number, port: DigitalPin) {
+            this.rows = rows
+            this.columns = columns
+            this.port = port
+            this.displayBuffer = pins.createBuffer(rows * columns * 3)
+        }
+    
+        /**
+         * Set powerbrick all pixels' color
+         * @param r pixel red intensity [0,255]
+         * @param g pixel green intensity [0,255]
+         * @param b pixel blue intensity [0,255]
+        */
+        public setAllPixels(r: number, g: number, b: number): void {
+            r = inRange(r, 0, 255)
+            g = inRange(g, 0, 255)
+            b = inRange(b, 0, 255)
+            for (let y = 0; y < this.rows; y++) {
+                for (let x = 0; x < this.columns; x++) {
+                    this.setPixelPrivate(y, x, r, g, b)
+                }
+            }
+        }
+        
+        /**
+         * Set pixel color
+         * @param y pixel y-coordinate [0,7]
+         * @param x pixel y-coordinate [0,7]
+         * @param r pixel red intensity [0,255]
+         * @param g pixel green intensity [0,255]
+         * @param b pixel blue intensity [0,255]
+        */
+       public setPixel(y: number, x: number, r: number, g: number, b: number): void {
+            y = inRange(y, 0, this.rows - 1)
+            x = inRange(x, 0, this.columns - 1)
+            r = inRange(r, 0, 255)
+            g = inRange(g, 0, 255)
+            b = inRange(b, 0, 255)
+            this.setPixelPrivate(y, x, r, g, b)
+        }
+        
+        private setPixelPrivate(y: number, x: number, r: number, g: number, b: number): void {
+            // 3 channels/sub-pixels (RGB) per pixel
+            let subPixelOffset = (y * this.columns + x) * 3
+            this.displayBuffer[subPixelOffset + 0] = g
+            this.displayBuffer[subPixelOffset + 1] = r
+            this.displayBuffer[subPixelOffset + 2] = b
+        }
+
+        /**
+         * Refresh pixels
+        */
+        public refresh() {
+            ws2812b.sendBuffer(this.displayBuffer, this.port);
+        }
+
+    }
+
+    /**
+     * Set powerbrick all pixels color
+     * @param y pixel y-coordinate [0,7]
+     * @param x pixel y-coordinate [0,7]
+     * @param r pixel red intensity [0,255]
+     * @param g pixel green intensity [0,255]
+     * @param b pixel blue intensity [0,255]
+    */
+    //% blockId=setPowerbrickAllPixels block="set powerbit pixel|%r|%g|%b"
+    //% weight=99
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="Powerbrick pixels"
+    export function setPowerbrickAllPixels(r: number, g: number, b: number): void {
+        if (initializedPowerbrickRgb == false)
+            return
+        powerblockPixels.setAllPixels(r, g, b)
+    }
+    
+    /**
+     * Set powerbrick pixel color
+     * @param y pixel y-coordinate [0,7]
+     * @param x pixel y-coordinate [0,7]
+     * @param r pixel red intensity [0,255]
+     * @param g pixel green intensity [0,255]
+     * @param b pixel blue intensity [0,255]
+    */
+    //% blockId=setPowerbrickPixel block="set powerbit pixel|%y|%x|%r|%g|%b"
+    //% weight=99
+    //% y.min=0 y.max=7 x.min=0 x.max=7 r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="Powerbrick pixels"
+    export function setPowerbrickPixel(y: number, x: number, r: number, g: number, b: number): void {
+        if (initializedPowerbrickRgb == false)
+            return
+        powerblockPixels.setPixel(y, x, r, g, b)
+    }
+
+    /**
+     * Refresh powerbrick pixels
+    */
+    //% blockId=refreshPowerbrickPixels block="refresh powerbit pixels|%port"
+    //% weight=99
+    //% port.min=0 port.max=6
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% blockId=refreshPowerbrickPixels block="Refresh powerbrick pixels"
+    //% group="Powerbrick pixels"
+    export function refreshPowerbrickPixels() {
+        if (initializedPowerbrickRgb == false)
+            return
+        powerblockPixels.refresh()
+    }
+
+    /**
+     * Set robotbit all pixels color
+     * @param y pixel y-coordinate [0,7]
+     * @param x pixel y-coordinate [0,7]
+     * @param r pixel red intensity [0,255]
+     * @param g pixel green intensity [0,255]
+     * @param b pixel blue intensity [0,255]
+    */
+    //% blockId=setPowerbrickAllPixels block="set powerbit pixel|%r|%g|%b"
+    //% weight=99
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="Robotbit pixels"
+    export function setRobotBitAllPixels(r: number, g: number, b: number): void {
+        if (initializedRobotbit == false)
+            return
+        robotbitPixels.setAllPixels(r, g, b)
+    }
+    
+    /**
+     * Set robotbit pixel color
+     * @param x pixel y-coordinate [0,7]
+     * @param r pixel red intensity [0,255]
+     * @param g pixel green intensity [0,255]
+     * @param b pixel blue intensity [0,255]
+    */
+    //% blockId=setPowerbrickPixel block="set powerbit pixel|%x|%r|%g|%b"
+    //% weight=99
+    //% x.min=0 x.max=7 r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% group="Robotbit pixels"
+    export function setRobotBitPixel(x: number, r: number, g: number, b: number): void {
+        if (initializedRobotbit == false)
+            return
+            robotbitPixels.setPixel(0, x, r, g, b)
+    }
+
+    /**
+     * Refresh robotbit pixels
+    */
+    //% blockId=refreshPowerbrickPixels block="refresh powerbit pixels|%port"
+    //% weight=99
+    //% port.min=0 port.max=6
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    //% blockId=refreshPowerbrickPixels block="Refresh powerbrick pixels"
+    //% group="Robotbit pixels"
+    export function refreshRobotBitPixels() {
+        if (initializedRobotbit == false)
+            return
+        robotbitPixels.refresh()
     }
 
 }
